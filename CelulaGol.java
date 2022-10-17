5f8c8cdfa8d7b07d4e48d8f4c9ffa7ca0707ebfa
GustavoZen <112349322+GustavoZen@users.noreply.github.com>
2022-10-17 20:41:36 -0300

    Add files via upload

---
+++ CelulaGol.java
@@ -0,0 +1,23 @@
+package Package;
+
+public class CelulaGol {
+	public int quadrante = 0;
+	public int x = 0;
+	public int y = 0;
+	public boolean situacao = false;
+	
+	public void setX(int a) {
+	x = a;
+	}
+	
+	public void setY(int b) {
+		y = b;
+	}
+	public void setSituacao(boolean a) {
+		situacao = a;
+	}
+	public void setQuadrante(int a) {
+		quadrante = a;
+	}
+}
+
